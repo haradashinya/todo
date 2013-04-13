70d82c81853986d5b6d71a4e5d04a99cabc860e1
okamurayasuyuki <haradashinya@gmail.com>
2013-04-13 21:38:21 +0900

    fix

---
+++ app.py
@@ -0,0 +1,14 @@
+#! coding: utf-8
+
+import sqlite3 as sq
+import sys
+from models.manager import Manager
+manager = Manager()
+manager.connect()
+
+
+
+
+
+
+
---
+++ models/manager.py
@@ -0,0 +1,16 @@
+import sqlite3 as lite
+class Manager():
+    def __init__(self):
+        print "init"
+    def connect(self):
+        con = lite.connect("todo.db")
+        with con:
+            cur = con.cursor()
+            cur.execute("select sqlite_version()")
+            data = cur.fetchone()
+            co = cur.fetchone()
+
+
+
+
+
